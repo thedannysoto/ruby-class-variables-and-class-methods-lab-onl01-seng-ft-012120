be8f73e8fc7aba6891f873b11b67d3c604db3bc1
Danny Soto <87dsoto@gmail.com>
2020-01-27 03:55:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,18 @@
 class Song 
   
   @@count = 0 
+  @@genres = []
   
   
-  def initialize
+  def initialize(name, artist, genre)
     @@count += 1 
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    
+    if @@genres.include?(@genre) == false
+      @@genres << @genre
+    end 
     
   end 
   
